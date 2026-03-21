dd7f969446597560336022398a3e0fa908985b3d
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-03-21 20:33:19 +0700

    Update footer styling

--- src/components/layouts/Footer.tsx
+++ src/components/layouts/Footer.tsx
@@ -40,7 +40,7 @@ const Footer: React.FC<FooterProps> = ({ bottomNavHeightPosition = 0 }) => {
         ref={footerRef}
         >
             <div className="px-4 flex flex-col justify-center items">
-                <div className='flex flex-col py-1 px-8'>
+                <div className='flex flex-col py-1 px-8 gap-10'>
                     <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center gap-6 sm:gap-8 lg:gap-12">
                         <div className='flex gap-10 justify-between w-full items-center py-3'>
                             <div className="flex flex-col items-center">
@@ -52,21 +52,21 @@ const Footer: React.FC<FooterProps> = ({ bottomNavHeightPosition = 0 }) => {
                                 </p>
                             </div>
 
-                            <div className="flex flex-col items-center lg:items-start">
+                            <div className="flex flex-col gap-1 items-center lg:items-start">
                                 <h3 className="text-white font-bold text-base sm:text-lg lg:text-xl">
-                                    Hubungi Developer
+                                    Hubungi Pengembang
                                 </h3>
-                                <div className="space-y-2 sm:space-y-3 w-full max-w-sm">
+                                <div className="space-y-3 sm:space-y-3 w-full max-w-sm group">
                                     <a 
                                         href="mailto:andreanjabar18@gmail.com"
-                                        className="flex items-center gap-2 rounded-lg text-white hover:bg-gray-700/50 transition-all duration-200 group p-2"
+                                        className="flex items-center gap-2 rounded-lg text-white hover:bg-gray-700/50 transition-all duration-200 group"
                                     >
-                                        <div className="w-9 h-9 sm:w-10 sm:h-10 lg:w-11 lg:h-11 bg-pingspot-hoverable rounded-lg flex items-center justify-center shadow-lg">
+                                        <div className="w-9 h-9 sm:w-10 sm:h-10 lg:w-11 lg:h-11 rounded-lg flex items-center justify-center shadow-lg">
                                             <SiGmail className="w-4 h-4 sm:w-5 sm:h-5 text-white" />
                                         </div>
                                         <div className="flex-1 min-w-0">
                                             <p className="text-xs text-gray-400">Email</p>
-                                            <p className="text-xs sm:text-sm lg:text-base font-medium truncate">
+                                            <p className="text-xs sm:text-sm lg:text-base font-medium truncate pr-1">
                                                 andreanjabar18@gmail.com
                                             </p>
                                         </div>
@@ -75,14 +75,14 @@ const Footer: React.FC<FooterProps> = ({ bottomNavHeightPosition = 0 }) => {
                                         href="https://github.com/AndreanDjabbar"
                                         target="_blank"
                                         rel="noopener noreferrer"
-                                        className="flex items-center gap-2 rounded-lg text-white hover:bg-gray-700/50 transition-all duration-200 group p-2"
+                                        className="flex items-center gap-2 rounded-lg text-white hover:bg-gray-700/50 transition-all duration-200 group"
                                     >
-                                        <div className="w-9 h-9 sm:w-10 sm:h-10 lg:w-11 lg:h-11 bg-pingspot-hoverable rounded-lg flex items-center justify-center shadow-lg">
+                                        <div className="w-9 h-9 sm:w-10 sm:h-10 lg:w-11 lg:h-11 rounded-lg flex items-center justify-center shadow-lg ">
                                             <FaGithub className="w-4 h-4 sm:w-5 sm:h-5 text-white" />
                                         </div>
                                         <div className="flex-1 min-w-0">
                                             <p className="text-xs text-gray-400">GitHub</p>
-                                            <p className="text-xs sm:text-sm lg:text-base font-medium">
+                                            <p className="text-xs sm:text-sm lg:text-base font-medium truncate pr-1">
                                                 @AndreanDjabbar
                                             </p>
                                         </div>
@@ -92,18 +92,20 @@ const Footer: React.FC<FooterProps> = ({ bottomNavHeightPosition = 0 }) => {
                         </div>
                     </div>
                     <div className="flex flex-col items-center justify-center">
-                        <div className="w-full border-t border-white/30 my-2"></div>
-                        <div className="flex flex-col sm:flex-row items-center justify-center gap-1 sm:gap-2 text-gray-300 text-xs sm:text-sm lg:text-base text-center">
+                        <div className="w-full border-t border-white/30 mb-2 sm:mb-7"></div>
+                        <div className="flex flex-col sm:flex-row items-center justify-center gap-1 sm:gap-2 text-gray-300 text-xs sm:text-sm lg:text-base mb-3 sm:mb-3">
                             <span>© {currentYear} PingSpot.</span>
-                            <span className="hidden sm:inline">Made by</span>
-                            <a
-                                href="https://github.com/AndreanDjabbar"
-                                target="_blank"
-                                rel="noopener noreferrer"
-                                className="font-bold text-white hover:text-sky-400 transition-colors duration-200"
-                            >
-                                Andrean Gusman Djabbar
-                            </a>
+                            <div>
+                                <span>Dikembangkan oleh </span>
+                                <a
+                                    href="https://github.com/AndreanDjabbar"
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                    className="font-bold text-white hover:border-b hover:border-white"
+                                >
+                                    Andrean Gusman Djabbar
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>

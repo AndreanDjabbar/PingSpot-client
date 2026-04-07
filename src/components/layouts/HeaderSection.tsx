60376e277f952ecb9a20f84aafd4f19c29b56130
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-04-07 21:11:23 +0700

    Add additonal props

--- src/components/layouts/HeaderSection.tsx
+++ src/components/layouts/HeaderSection.tsx
@@ -5,18 +5,45 @@ interface HeaderSectionProps {
     currentPath: string;
     message: string;
     children?: React.ReactNode;
+    isCardHeader?: boolean;
+    showBreadcrumb?: boolean;
 }
+
+const paths = [
+    { id: 'home', label: '🏠 Beranda',},
+    { id: 'map', label: 'Peta Interaktif', },
+    { id: 'explore', label: '🔍 Jelajahi', },
+    { id: 'community', label: 'Komunitas', },
+    { id: 'messages', label: 'Pesan', },
+    { id: 'activity', label: 'Aktivitas', },
+    { id: 'settings', label: '⚙️ Pengaturan', },
+    { id: 'help', label: 'Bantuan' },
+    { id: 'profile', label: 'Profil' },
+    { id: 'notifications', label: '🔔 Notifikasi' },
+    { id: 'security', label: 'Keamanan' },
+    { id: 'reports', label: '📝 Laporan' },
+    { id: 'create-report', label: 'Buat Laporan' },
+]
 
 const HeaderSection: React.FC<HeaderSectionProps> = ({
     currentPath,
     message = "Kelola pengaturan akun dan preferensi Anda di sini.",
     children,
+    isCardHeader = true,
+    showBreadcrumb = true,
 }) => {
+    const currentPathParts = currentPath.split("/").filter(Boolean);
     return (
-        <div className="bg-white rounded-lg border border-gray-200 shadow-sm p-6">
+        <div className={`${isCardHeader ? 'p-6 bg-white rounded-lg border border-gray-200 shadow-sm' : 'py-6'}`}>
             <div className="flex flex-col lg:flex-row lg:items-center justify-between gap-6">
                 <div className='flex flex-col gap-3'>
-                    <Breadcrumb path={currentPath}/>
+                    {showBreadcrumb ? (
+                        <Breadcrumb path={currentPath}/>
+                    ) : (
+                        <h1 className="text-2xl font-bold text-gray-900">
+                            {paths.find((p) => p.id === currentPathParts[1])?.label || (currentPathParts.length > 0 ? currentPathParts[currentPathParts.length - 1].charAt(0).toUpperCase() + currentPathParts[currentPathParts.length - 1].slice(1) : 'Dashboard')}
+                        </h1>
+                    )}
                     <p className="text-gray-600 text-sm">
                         {message}
                     </p>

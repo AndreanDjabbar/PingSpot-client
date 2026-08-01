a776231a523569ea5952b5f7365ae18d36744513
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-08-01 19:51:34 +0700

    Fix follow route

--- src/services/social.service.ts
+++ src/services/social.service.ts
@@ -2,7 +2,7 @@ import axiosInstance from "@/lib/axiosInstance";
 import { IFollowResponse, IGetFollowDataResponse } from "@/types";
 
 export const followService = async (followID: number, followingType: 'user' | 'community'): Promise<IFollowResponse> => {
-    const response = await axiosInstance.post<IFollowResponse>(`/user/follow`, { followID, followingType }, {
+    const response = await axiosInstance.post<IFollowResponse>(`/social/follow`, { followID, followingType }, {
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
@@ -12,6 +12,6 @@ export const followService = async (followID: number, followingType: 'user' | 'c
 }
 
 export const getFollowDataService = async (followingID: number, followingType: 'user' | 'community'): Promise<IGetFollowDataResponse> => {
-    const response = await axiosInstance.get<IGetFollowDataResponse>(`/user/follow/${followingID}/${followingType}`);
+    const response = await axiosInstance.get<IGetFollowDataResponse>(`/social/follow/${followingID}/${followingType}`);
     return response.data;
 }

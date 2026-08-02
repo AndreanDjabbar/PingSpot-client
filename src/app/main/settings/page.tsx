e8cedeb8414a92a3fe63cd9fe105f3d699d25f5f
Andrean Gusman Djabbar <andreanjabar18@gmail.com>
2026-08-02 20:14:06 +0700

    Add profile badge

--- src/app/main/settings/page.tsx
+++ src/app/main/settings/page.tsx
@@ -5,13 +5,13 @@ export const dynamic = 'force-dynamic';
 import React, { useEffect, useState } from 'react';
 import { BiLock, BiEnvelope, BiUser, BiCog } from 'react-icons/bi';
 import { MdOutlineLanguage, MdOutlineMarkEmailUnread } from 'react-icons/md';
-import { useErrorToast, useSuccessToast, useLogout } from '@/hooks';
+import { useErrorToast, useSuccessToast, useLogout, useGetFollowData } from '@/hooks';
 import { useRouter, usePathname } from 'next/navigation';
 import { ImExit } from 'react-icons/im';
 import { IoIosNotifications } from "react-icons/io";
 import { useUserProfileStore, useConfirmationModalStore } from '@/stores';
 import { SettingCard, SettingItem } from './components';
-import { Button, ToggleSwitch, HeaderSection } from '@/components';
+import { Button, ToggleSwitch, HeaderSection, ProfileBadge } from '@/components';
 
 const SettingsPage = () => {
     const router = useRouter();
@@ -23,6 +23,15 @@ const SettingsPage = () => {
 
     const user = useUserProfileStore(state => state.userProfile);
     const openConfirm = useConfirmationModalStore((state) => state.openConfirm);
+
+    const {
+        isPending: isFetchingFollowData,
+        isError: isErrorFetchingFollowData,
+        error: errorFetchingFollowData,
+        data: followData
+    } = useGetFollowData(Number(user?.userID) || 0, 'user');
+    const followingCount = followData?.data?.followingCount || 0;
+    const followersCount = followData?.data?.followersCount || 0;
 
     const { mutate: logout, isPending, isError, error, isSuccess, data } = useLogout();
 
@@ -57,6 +66,7 @@ const SettingsPage = () => {
 
     useErrorToast(isError, error);
     useSuccessToast(isSuccess, data);
+    useErrorToast(isErrorFetchingFollowData, errorFetchingFollowData);
 
     useEffect(() => {
         if (isSuccess) {
@@ -76,40 +86,61 @@ const SettingsPage = () => {
 
             <div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
                 <SettingCard title="Akun & Profil" icon={BiUser}>
-                <div className="space-y-2">
-                    <SettingItem
-                    title="Profil Pengguna"
-                    description="Ubah informasi profil, foto, dan preferensi Anda"
-                    icon={BiUser}
-                    action={
-                        <Button
-                        onClick={() => router.push('/main/settings/profile')}
-                        className="px-3 py-1 text-sm w-full md:w-auto"
-                        variant='outline'
-                        >
-                            Ubah
+                <div className="flex flex-col gap-6">
+                    <div className='p-4 rounded-lg flex flex-col gap-4 border border-muted '>
+                        <ProfileBadge 
+                        name={user?.username || ''}
+                        followers={isFetchingFollowData ? undefined : followersCount}
+                        following={isFetchingFollowData ? undefined : followingCount}
+                        imageUrl={user?.profilePicture}       
+                        email={user?.email || 'Andreanjabar19@gmail.com'}                 
+                        textColors={{ 
+                            name: 'text-gray-900', 
+                            email: 'text-gray-500', 
+                            profileRing: 'ring-gray-300',
+                            followers: 'text-gray-900',
+                            following: 'text-gray-900', 
+                        }}
+                        />
+                        <Button className='border border-gray-300 text-gray-900 w-fit bg-white hover:bg-gray-100 transition-all duration-200' onClick={() => router.push(`/main/profile/${user?.username}`)}>
+                            Lihat Profil
                         </Button>
-                    }
-                    />
-                    <SettingItem
-                    title="Keamanan Akun"
-                    description="Ubah password dan pengaturan keamanan"
-                    icon={BiLock}
-                    action={
-                        <Button
-                        onClick={() => router.push('/main/settings/security')}
-                        className="px-3 py-1 text-sm w-full"
-                        variant='outline'
-                        >
-                            Ubah
-                        </Button>
-                    }
-                    />
-                    <SettingItem
-                    title="Email"
-                    description={user?.email || ''}
-                    icon={BiEnvelope}
-                    />
+                    </div>
+                    <div className=''>
+                        <SettingItem
+                        title="Edit Profil Pengguna"
+                        description="Edit informasi profil, foto, dan preferensi Anda"
+                        icon={BiUser}
+                        action={
+                            <Button
+                            onClick={() => router.push('/main/settings/profile')}
+                            className="px-3 py-1 text-sm w-full md:w-auto"
+                            variant='outline'
+                            >
+                                Edit
+                            </Button>
+                        }
+                        />
+                        <SettingItem
+                        title="Keamanan Akun"
+                        description="Ubah password dan pengaturan keamanan"
+                        icon={BiLock}
+                        action={
+                            <Button
+                            onClick={() => router.push('/main/settings/security')}
+                            className="px-3 py-1 text-sm w-full"
+                            variant='outline'
+                            >
+                                Edit
+                            </Button>
+                        }
+                        />
+                        <SettingItem
+                        title="Email"
+                        description={user?.email || ''}
+                        icon={BiEnvelope}
+                        />
+                    </div>
                 </div>
                 </SettingCard>
 

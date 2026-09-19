8b7dd23d8085e7fdf31e92d7d3a1b8078aa37d09
AndreanDjabbar <andreanjabar18@gmail.com>
2026-09-19 18:18:58 +0700

    Add additional field

--- src/types/model/report.ts
+++ src/types/model/report.ts
@@ -159,6 +159,9 @@ export interface IGetReportSaved {
     reportSavedID: number;
     reportID: number;
     userID: number;
+    userName: string;
+    fullName: string;
+    profilePicture?: string;
     reportTitle: string;
     reportType: ReportType;
     reportDescription: string;
